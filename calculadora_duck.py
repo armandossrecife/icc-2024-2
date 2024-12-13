f6ca3d5decd5d81349b5abce291b29132d5370d0
Armando Soares Sousa <armando.sousa@gmail.com>
2024-12-13 16:28:03 -0300

    Create calculadora_duck.py

---
+++ calculadora_duck.py
@@ -0,0 +1,33 @@
+# Programa Calculadora NAO modularizado
+
+print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+print("Bem vindo ao programa mini-calculadora")
+print("Este programa faz a soma, multiplicação, subtração e divisão")
+print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+
+print("Soma dois numeros")
+numero1 = float(input("Informe o primeiro número:"))
+numero2 = float(input("Informe o seguendo número:"))
+soma = numero1 + numero2
+print("A soma dos números é:",soma)
+print("-------------------------------")
+
+print("Multiplica dois numeros")
+numero1 = float(input("Informe o primeiro número:"))
+numero2 = float(input("Informe o seguendo número:"))
+produto = numero1 * numero2
+print("O produto dos números é:",produto)
+print("-------------------------------")
+
+print("Subtrai dois numeros")
+numero1 = float(input("Informe o primeiro número:"))
+numero2 = float(input("Informe o seguendo número:"))
+subtracao = numero1 - numero2
+print("A subtração dos números é:",subtracao)
+print("-------------------------------")
+
+print("Divisão de dois numeros")
+numero1 = float(input("Informe o primeiro número:"))
+numero2 = float(input("Informe o seguendo número:"))
+divisao = numero1 / numero2
+print("A divisão dos números é:",divisao)
